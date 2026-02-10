92daf433bda320708b72377a69e554607c0ac5b5
Mohamed Taymour <mohamedtaymour1@gmail.com>
2026-02-10 09:01:21 +0400

    Added Main Menu UI that works

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,10 +1,36 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+    public void Start()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
+
+    public void OptionsPress()
+    {
+        optionsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void BackfromOptionMenu(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            optionsMenu.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+    }    
 }

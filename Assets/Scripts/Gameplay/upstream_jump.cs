f0abd6780f29ee6a71ef83a4f5f4ddf21b977ba1
GadoXZ <162186731+GadoXZ@users.noreply.github.com>
2026-02-22 07:50:07 +0300

    added sound

--- Assets/Scripts/Gameplay/upstream_jump.cs
+++ Assets/Scripts/Gameplay/upstream_jump.cs
@@ -3,7 +3,13 @@ using UnityEngine;
 public class JumpPad : MonoBehaviour
 {
     public float jumpForce = 25f;
-
+    private AudioManager audioManager;
+    private void Awake()
+    {
+        audioManager = FindFirstObjectByType<AudioManager>();
+        if (audioManager == null)
+            Debug.LogError("AudioManager not found!");
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -12,6 +18,10 @@ public class JumpPad : MonoBehaviour
             {
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
                 rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                if(audioManager!= null)
+                    {
+                        audioManager.Play("UpStream");
+                    }
             }
         }
     }
